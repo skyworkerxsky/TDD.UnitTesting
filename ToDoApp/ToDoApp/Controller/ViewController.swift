2ad7152b23f5f3a2db07a95129bfc6e22bf1aa2f
skyworkerxsky <pwuser@yandex.ru>
2020-02-03 19:11:24 +0500

    add new prjct

---
+++ ToDoApp/ToDoApp/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Алексей Макаров on 03.02.2020.
+//  Copyright © 2020 Алексей Макаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
