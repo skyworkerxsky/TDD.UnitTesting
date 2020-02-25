b51a0be80d5198ac462ff86524f8ad2bcb2ef39c
skyworkerxsky <pwuser@yandex.ru>
2020-02-25 22:05:54 +0500

    Data Provider tests part 2

---
+++ ToDoApp/ToDoApp/View/TaskCell.swift
@@ -0,0 +1,15 @@
+//
+//  TaskCell.swift
+//  ToDoApp
+//
+//  Created by Алексей Макаров on 25.02.2020.
+//  Copyright © 2020 Алексей Макаров. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    
+    
+}
--- ToDoApp/ToDoAppTests/Controller/DataProviderTests.swift
+++ ToDoApp/ToDoAppTests/Controller/DataProviderTests.swift
@@ -13,17 +13,19 @@ class DataProviderTests: XCTestCase {
     
     var sut: DataProvider!
     var tableView: UITableView!
-
+    
     override func setUp() {
         sut = DataProvider()
+        sut.taskManager = TaskManager()
+        
         tableView = UITableView()
         tableView.dataSource = sut
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     // проверяем что у нас 2 секции (задачи и выполненные задачи)
     func testNumberOfSectionsIsTwo() {
         
@@ -32,9 +34,9 @@ class DataProviderTests: XCTestCase {
         XCTAssertEqual(numberOfSections, 2)
     }
     
-    func testNumberOfRowsInSectionZeroIsTaskCount() {
-        
-        sut.taskManager = TaskManager()
+    
+    // проверяем что кол-во строк в секции совпадает после добавления задач
+    func testNumberOfRowsInSectionZeroIsTasksCount() {
         
         sut.taskManager?.add(task: Task(title: "Foo"))
         
@@ -46,5 +48,31 @@ class DataProviderTests: XCTestCase {
         
         XCTAssertEqual(tableView.numberOfRows(inSection: 0), 2)
     }
-
+    
+    // проверяем что кол-во строк в секции "выполненных" совпадает после добавления задач
+    func testNumberOfRowsInSectionOneIsDoneTasksCount() {
+        
+        sut.taskManager?.add(task: Task(title: "Foo"))
+        sut.taskManager?.checkTask(at: 0)
+        
+        XCTAssertEqual(tableView.numberOfRows(inSection: 1), 1)
+        
+        sut.taskManager?.add(task: Task(title: "Bar"))
+        sut.taskManager?.checkTask(at: 0)
+        
+        tableView.reloadData() // перезагружаем таблицу в ручную
+        
+        XCTAssertEqual(tableView.numberOfRows(inSection: 1), 2)
+    }
+    
+    // провеяем какую ячейку получаем в методе indexPath
+    func testCellForRowAtIndexPathReturnsTaskCell() {
+        sut.taskManager?.add(task: Task(title: "Foo"))
+        tableView.reloadData()
+        
+        let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 0))
+        
+        XCTAssertTrue(cell is TaskCell)
+    }
+    
 }
