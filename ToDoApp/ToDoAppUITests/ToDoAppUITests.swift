fe5a58b85ed2f25fc552af02028b71c3c5fb2593
skyworkerxsky <pwuser@yandex.ru>
2020-03-14 02:33:53 +0500

    finish tests

--- ToDoApp/ToDoAppUITests/ToDoAppUITests.swift
+++ ToDoApp/ToDoAppUITests/ToDoAppUITests.swift
@@ -29,7 +29,7 @@ class ToDoAppUITests: XCTestCase {
         app.launch()
 
         // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
     }
 
     func testLaunchPerformance() {
